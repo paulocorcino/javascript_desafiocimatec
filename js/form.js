267647153e0cdc3abf558a9c71a23bdd652c233d
Paulo Sousa <paulo.sousa@hp.com>
2021-09-03 13:23:09 -0300

    finished

---
+++ js/form.js
@@ -0,0 +1,33 @@
+
+//class contato
+
+class contato {
+    nome;
+    sobrenome;
+    email;
+    cpf;
+    telefone;
+    contato;
+    constructor (nome, sobrenome, email, cpf, telefone, contato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
+    }
+}
+
+function Post(form) {
+
+  let data = new contato(form.elements.namedItem("nome").value,
+            form.elements.namedItem("sobrenome").value, 
+            form.elements.namedItem("email").value, 
+            form.elements.namedItem("cpf").value, 
+            form.elements.namedItem("telefone").value, 
+            form.elements.namedItem("contato").value);
+
+    //  done
+    alert("Obrigado!");
+  
+}
